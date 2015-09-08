a5d3f9bd6467c263160f363e95a2f5db3c5bbf6a
Daniele Venzano <venza@brownhat.org>
2015-09-08 14:34:22 +0200

    Use javascript to format timestamps according to the browser locale and timezone

--- zoe_web/web/__init__.py
+++ zoe_web/web/__init__.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask import Blueprint
 
 web_bp = Blueprint('web', __name__, template_folder='templates', static_folder='static')
@@ -8,11 +6,3 @@ import zoe_web.web.start
 import zoe_web.web.status
 import zoe_web.web.applications
 
-
-@web_bp.app_template_filter('format_timestamp')
-def _jinja2_filter_datetime(timestamp):
-    try:
-        dt = datetime.fromtimestamp(timestamp)
-    except TypeError:
-        return timestamp
-    return dt.ctime()
