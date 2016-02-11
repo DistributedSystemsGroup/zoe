748a45d735ddc05693fa1ce3bccd4ad29ca312c7
Daniele Venzano <venza@brownhat.org>
2016-02-11 10:41:29 +0100

    Remove outdated sample script

--- example_use.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-P3=`which python3`
-
-$P3 ./zoe.py user-new venzano@eurecom.fr
-$P3 ./zoe.py app-new --user-id 1 tests/resources/spark-notebook-test.json
-$P3 ./zoe.py start 1
-
