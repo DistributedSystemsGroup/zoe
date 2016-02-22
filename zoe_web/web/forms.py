5418f96cdc22463865277e7cc73f3280e6642971
Daniele Venzano <venza@brownhat.org>
2016-02-22 14:58:46 +0100

    Guest IDs can be 4 to 16 characters long

--- zoe_web/web/forms.py
+++ zoe_web/web/forms.py
@@ -2,4 +2,4 @@ from wtforms import Form, StringField, validators
 
 
 class LoginWithGuestIDForm(Form):
-    guest_identifier = StringField('Guest ID', [validators.Length(min=8, max=8)])
+    guest_identifier = StringField('Guest ID', [validators.Length(min=4, max=16)])
