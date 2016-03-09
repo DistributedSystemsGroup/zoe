e40377126683f8de541376a3da229766e0837b84
Daniele Venzano <venza@brownhat.org>
2016-03-09 19:28:09 +0100

    Fix the observer process to forget about dead containers that no longer exist in Zoe

--- zoe_observer/swarm_event_manager.py
+++ zoe_observer/swarm_event_manager.py
@@ -2,6 +2,7 @@ import logging
 
 from zoe_observer.config import get_conf
 from zoe_lib.containers import ZoeContainerAPI
+from zoe_lib.exceptions import ZoeAPIException
 
 log = logging.getLogger(__name__)
 
@@ -30,4 +31,8 @@ def container_died(zoe_id: int):
     log.debug('A container died')
     # tell the scheduler via the rest api
     cont_api = ZoeContainerAPI(get_conf().scheduler_url, 'zoeadmin', get_conf().zoeadmin_password)
-    cont_api.died(zoe_id)
+    try:
+        cont_api.died(zoe_id)
+    except ZoeAPIException as e:
+        if e.message != "No such container":
+            log.exception('Error reporting a dead container')
