d5c1900da508d5f182f1d6a8b30c113da45bbcd6
Daniele Venzano <venza@brownhat.org>
2016-03-18 18:50:10 +0100

    Clean the sample usage script

--- basic_usage.sh
+++ basic_usage.sh
@@ -16,9 +16,3 @@ echo "Delete a user"
 ./zoe.py user-rm test
 echo "Export an application template"
 ./zoe.py pre-app-export hdfs > /tmp/zoe-hdfs.json
-echo "Upload the template as a new application"
-./zoe.py app-new /tmp/zoe-hdfs.json
-echo "Get the application back from Zoe"
-./zoe.py app-get hdfs
-echo "Delete the app"
-./zoe.py app-rm hdfs
