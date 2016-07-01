209bd09afb914151309a62780dbd6feb8f8a9c45
Daniele Venzano <venza@brownhat.org>
2016-07-01 16:30:17 +0200

    Move configuration to a common shared module

--- zoe_lib/config.py
+++ zoe_lib/config.py
@@ -16,8 +16,8 @@
 from zoe_lib.configargparse import ArgumentParser, Namespace
 
 config_paths = [
-    'zoe-master.conf',
-    '/etc/zoe/zoe-master.conf'
+    'zoe.conf',
+    '/etc/zoe/zoe.conf'
 ]
 
 # Singletons
@@ -37,15 +37,25 @@ _conf = None
 def load_configuration(test_conf=None):
     global _conf
     if test_conf is None:
-        argparser = ArgumentParser(description="Zoe Master - Container Analytics as a Service core component",
+        argparser = ArgumentParser(description="Zoe - Container Analytics as a Service",
                                    default_config_files=config_paths,
-                                   auto_env_var_prefix="ZOE_MASTER_",
+                                   auto_env_var_prefix="ZOE_",
                                    args_for_setting_config_path=["--config"],
                                    args_for_writing_out_config_file=["--write-config"])
+
+        # Common options
         argparser.add_argument('--debug', action='store_true', help='Enable debug output')
         argparser.add_argument('--swarm', help='Swarm/Docker API endpoint (ex.: zk://zk1:2181,zk2:2181 or http://swarm:2380)', default='http://localhost:2375')
-        argparser.add_argument('--api-listen-uri', help='ZMQ API listen address', default='tcp://*:4850')
         argparser.add_argument('--deployment-name', help='name of this Zoe deployment', default='prod')
+
+        argparser.add_argument('--dbname', help='DB name', default='zoe')
+        argparser.add_argument('--dbuser', help='DB user', default='zoe')
+        argparser.add_argument('--dbpass', help='DB password', default='zoe')
+        argparser.add_argument('--dbhost', help='DB hostname', default='localhost')
+        argparser.add_argument('--dbport', type=int, help='DB port', default=5432)
+
+        # Master options
+        argparser.add_argument('--api-listen-uri', help='ZMQ API listen address', default='tcp://*:4850')
         argparser.add_argument('--influxdb-dbname', help='Name of the InfluxDB database to use for storing metrics', default='zoe')
         argparser.add_argument('--influxdb-url', help='URL of the InfluxDB service (ex. http://localhost:8086)', default='http://localhost:8086')
         argparser.add_argument('--influxdb-enable', action="store_true", help='Enable metric output toward influxDB')
@@ -53,11 +63,18 @@ def load_configuration(test_conf=None):
         argparser.add_argument('--workspace-base-path', help='Path where user workspaces will be created by Zoe. Must be visible at this path on all Swarm hosts.', default='/mnt/zoe-workspaces')
         argparser.add_argument('--overlay-network-name', help='Name of the Swarm overlay network Zoe should use', default='zoe')
 
-        argparser.add_argument('--dbname', help='DB name', default='zoe')
-        argparser.add_argument('--dbuser', help='DB user', default='zoe')
-        argparser.add_argument('--dbpass', help='DB password', default='zoe')
-        argparser.add_argument('--dbhost', help='DB hostname', default='localhost')
-        argparser.add_argument('--dbport', type=int, help='DB port', default=5432)
+        # API options
+        argparser.add_argument('--listen-address', type=str, help='Address to listen to for incoming connections', default="0.0.0.0")
+        argparser.add_argument('--listen-port', type=int, help='Port to listen to for incoming connections', default=5001)
+        argparser.add_argument('--master-url', help='URL of the Zoe master process', default='tcp://127.0.0.1:4850')
+
+        argparser.add_argument('--ldap-server-uri', help='LDAP server to use for authentication', default='ldap://localhost')
+        argparser.add_argument('--ldap-base-dn', help='LDAP base DN for users', default='ou=something,dc=any,dc=local')
+        argparser.add_argument('--ldap-bind-user', help='LDAP user to bind as for user lookup', default='cn=guest,dc=bigfoot,dc=eurecom,dc=fr')
+        argparser.add_argument('--ldap-bind-password', help='LDAP user password', default='notsosecret')
+        argparser.add_argument('--ldap-admin-gid', type=int, help='LDAP group ID for admins', default=5000)
+        argparser.add_argument('--ldap-user-gid', type=int, help='LDAP group ID for users', default=5001)
+        argparser.add_argument('--ldap-guest-gid', type=int, help='LDAP group ID for guests', default=5002)
 
         opts = argparser.parse_args()
         if opts.debug:
