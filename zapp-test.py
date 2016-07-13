cb1cd103ccc725226c60e50e4a8780bbcf9f3339
Daniele Venzano <venza@brownhat.org>
2016-07-13 10:38:05 +0200

    Update configuration for the zapp_test tool

--- zapp-test.py
+++ zapp-test.py
@@ -128,42 +128,60 @@ class FakeSQLManager:
         return self._last_id - 1
 
 
-def load_configuration(test_conf=None):
+def load_configuration():
     """Load configuration from the command line."""
-    if test_conf is None:
-        argparser = ArgumentParser(description="Zoe application tester - Container Analytics as a Service core component",
-                                   default_config_files=CONFIG_PATHS,
-                                   auto_env_var_prefix="ZOE_MASTER_",
-                                   args_for_setting_config_path=["--config"],
-                                   args_for_writing_out_config_file=["--write-config"])
-        argparser.add_argument('--debug', action='store_true', help='Enable debug output')
-        argparser.add_argument('--swarm', help='Swarm/Docker API endpoint (ex.: zk://zk1:2181,zk2:2181 or http://swarm:2380)', default='http://localhost:2375')
-        argparser.add_argument('--api-listen-uri', help='ZMQ API listen address', default='tcp://*:4850')
-        argparser.add_argument('--deployment-name', help='name of this Zoe deployment', default='prod')
-        argparser.add_argument('--influxdb-dbname', help='Name of the InfluxDB database to use for storing metrics', default='zoe')
-        argparser.add_argument('--influxdb-url', help='URL of the InfluxDB service (ex. http://localhost:8086)', default='http://localhost:8086')
-        argparser.add_argument('--influxdb-enable', action="store_true", help='Enable metric output toward influxDB')
-        argparser.add_argument('--gelf-address', help='Enable Docker GELF log output to this destination (ex. udp://1.2.3.4:1234)', default='')
-        argparser.add_argument('--workspace-base-path', help='Path where user workspaces will be created by Zoe. Must be visible at this path on all Swarm hosts.', default='/mnt/zoe-workspaces')
-        argparser.add_argument('--overlay-network-name', help='Name of the Swarm overlay network Zoe should use', default='zoe')
+    argparser = ArgumentParser(description="Zoe application tester - Container Analytics as a Service core component",
+                               default_config_files=CONFIG_PATHS,
+                               auto_env_var_prefix="ZOE_MASTER_",
+                               args_for_setting_config_path=["--config"],
+                               args_for_writing_out_config_file=["--write-config"])
 
-        argparser.add_argument('--dbname', help='DB name', default='zoe')
-        argparser.add_argument('--dbuser', help='DB user', default='zoe')
-        argparser.add_argument('--dbpass', help='DB password', default='zoe')
-        argparser.add_argument('--dbhost', help='DB hostname', default='localhost')
-        argparser.add_argument('--dbport', type=int, help='DB port', default=5432)
+    argparser.add_argument('--debug', action='store_true', help='Enable debug output')
+    argparser.add_argument('--swarm', help='Swarm/Docker API endpoint (ex.: zk://zk1:2181,zk2:2181 or http://swarm:2380)', default='http://localhost:2375')
+    argparser.add_argument('--deployment-name', help='name of this Zoe deployment', default='prod')
 
-        argparser.add_argument('jsonfile', type=FileType("r"), help='Application description')
+    argparser.add_argument('--dbname', help='DB name', default='zoe')
+    argparser.add_argument('--dbuser', help='DB user', default='zoe')
+    argparser.add_argument('--dbpass', help='DB password', default='')
+    argparser.add_argument('--dbhost', help='DB hostname', default='localhost')
+    argparser.add_argument('--dbport', type=int, help='DB port', default=5432)
 
-        opts = argparser.parse_args()
+    # Master options
+    argparser.add_argument('--api-listen-uri', help='ZMQ API listen address', default='tcp://*:4850')
+    argparser.add_argument('--influxdb-dbname', help='Name of the InfluxDB database to use for storing metrics', default='zoe')
+    argparser.add_argument('--influxdb-url', help='URL of the InfluxDB service (ex. http://localhost:8086)', default='http://localhost:8086')
+    argparser.add_argument('--influxdb-enable', action="store_true", help='Enable metric output toward influxDB')
+    argparser.add_argument('--gelf-address', help='Enable Docker GELF log output to this destination (ex. udp://1.2.3.4:1234)', default='')
+    argparser.add_argument('--workspace-base-path', help='Path where user workspaces will be created by Zoe. Must be visible at this path on all Swarm hosts.', default='/mnt/zoe-workspaces')
+    argparser.add_argument('--overlay-network-name', help='Name of the Swarm overlay network Zoe should use', default='zoe')
 
-        opts.gelf_address = ''  # For debugging we want to easily look at logs with 'docker logs'
-        opts.influxdb_enable = False  # don't send metrics for these test runs
+    # API options
+    argparser.add_argument('--listen-address', type=str, help='Address to listen to for incoming connections', default="0.0.0.0")
+    argparser.add_argument('--listen-port', type=int, help='Port to listen to for incoming connections', default=5001)
+    argparser.add_argument('--master-url', help='URL of the Zoe master process', default='tcp://127.0.0.1:4850')
 
-        if opts.debug:
-            argparser.print_values()
+    # API auth options
+    argparser.add_argument('--auth-type', help='Authentication type (text or ldap)', default='text')
 
-        return opts
+    argparser.add_argument('--auth-file', help='Path to the CSV file containing user,pass,role lines for text authentication', default='zoepass.csv')
+
+    argparser.add_argument('--ldap-server-uri', help='LDAP server to use for authentication', default='ldap://localhost')
+    argparser.add_argument('--ldap-base-dn', help='LDAP base DN for users', default='ou=something,dc=any,dc=local')
+    argparser.add_argument('--ldap-admin-gid', type=int, help='LDAP group ID for admins', default=5000)
+    argparser.add_argument('--ldap-user-gid', type=int, help='LDAP group ID for users', default=5001)
+    argparser.add_argument('--ldap-guest-gid', type=int, help='LDAP group ID for guests', default=5002)
+
+    argparser.add_argument('jsonfile', type=FileType("r"), help='Application description')
+
+    opts = argparser.parse_args()
+
+    opts.gelf_address = ''  # For debugging we want to easily look at logs with 'docker logs'
+    opts.influxdb_enable = False  # don't send metrics for these test runs
+
+    if opts.debug:
+        argparser.print_values()
+
+    return opts
 
 
 def main():
