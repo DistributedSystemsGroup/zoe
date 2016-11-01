1ef8cc5a6508646850b572335145f6a872deaeab
hxquangnhat <hxquangnhat@gmail.com>
2016-11-01 18:35:30 +0100

    add new rest api for getting user info

---
+++ zoe_api/rest_api/userinfo.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2016, Daniele Venzano
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Info API endpoint."""
+
+from tornado.web import RequestHandler
+from zoe_api.rest_api.utils import get_auth, catch_exceptions
+
+
+class UserInfoAPI(RequestHandler):
+    """The UserInfo API endpoint."""
+
+    def initialize(self, **kwargs):
+        """Initializes the request handler."""
+        self.api_endpoint = kwargs['api_endpoint']  # type: APIEndpoint
+
+    @catch_exceptions
+    def get(self):
+        """HTTP GET method."""
+        uid, role = get_auth(self)
+
+        ret = {
+            'uid': uid,
+            'role': role
+        }
+
+        self.write(ret)
+
+    def data_received(self, chunk):
+        """Not implemented as we do not use stream uploads"""
+        pass
