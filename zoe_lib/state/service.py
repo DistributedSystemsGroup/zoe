a8763217b021665c50aee558375139a0014ad8b6
qhoangxuan <qhoangxuan@kpmg.com>
2017-06-21 10:46:03 +0200

    fix underscore volume init

--- zoe_lib/state/service.py
+++ zoe_lib/state/service.py
@@ -70,7 +70,7 @@ class VolumeDescription:
 class VolumeDescriptionHostPath(VolumeDescription):
     """Host-based volumes."""
     def __init__(self, name: str, path: str, readonly: bool):
-        super().__init__("host-directory")
+        super().__init__("host_directory")
         self.path = path
         self.mount_point = '/mnt/' + name
         self.readonly = readonly
