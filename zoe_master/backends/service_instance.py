bca4095752a57f2423414c21f59737405c333fcf
Daniele Venzano <venza@brownhat.org>
2017-09-19 12:06:45 +0200

    Remove unused import

--- zoe_master/backends/service_instance.py
+++ zoe_master/backends/service_instance.py
@@ -17,7 +17,7 @@
 
 import collections
 
-from zoe_lib.state import Service, Execution, VolumeDescriptionHostPath
+from zoe_lib.state import Service, Execution
 from zoe_lib.config import get_conf
 import zoe_master.backends.common
 
